feafee55e6c86bde6ce5cad3895311f6bdbd3945
baoruichen <1152383857@qq.com>
2022-02-08 17:45:22 +0800

    step1
    通过改变头尾坐标，实现小蛇移动

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 记录寒假学习
 2022/1/27
 https://weblab.mit.edu/
+
+#### w0
+css文件中用：
+- .来定义class
+- #来定义id
+- :root来定义变量
+
+每个元素从外到内为margin-border-padding
+
+#### w1
--- w1/game.js
+++ w1/game.js
@@ -1,12 +1,18 @@
+const gameBoard = document.getElementById("game-board");
+
 const main = () => {
     update();
     draw();
 }
 
-setInterval(main);
+setInterval(main, 1000/SNAKE_SPEED);
 
 const update = () => {
+    console.log("Updating");
+    updateSnake();
 }
 
 const draw = () => {
+    gameBoard.innerHTML = "";
+    drawSnake(gameBoard);
 }
